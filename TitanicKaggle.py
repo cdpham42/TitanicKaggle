756c0b97225712632c03b236a4761c37d129adcc
cdpham42 <cdpham42@gmail.com>
2019-02-25 15:42:47 -0800

    Add data and initialize TitanicKaggle.py

---
+++ TitanicKaggle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 25 15:40:04 2019
+
+@author: Casey Pham
+
+Titanic Kaggle Challenge
+
+Machine learning exercise to intro into Kaggle, and to practice basic data 
+science and machine learning techniques.
+"""
+
